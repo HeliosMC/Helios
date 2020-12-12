110ba0d3f223288faa4800512de90d994e8aa93f
Luaa <datorugames@gmail.com>
2020-12-12 13:25:33 +0000

    Change 8ball responses.

--- commands/fun/8ball.js
+++ commands/fun/8ball.js
@@ -36,13 +36,26 @@ module.exports = {
 
         // Get a random response.
         let responses = [
-            "No.",
+            "It is certain.",
+            "It is decidedly so.",
+            "Without a doubt.",
+            "Yes – definitely.",
+            "You may rely on it.",
+            "As I see it, yes.",
+            "Most likely.",
+            "Outlook good.",
             "Yes.",
-            "I don't think so.",
-            "Maybe.",
-            "Definitely, not.",
-            "Yes, definitely.",
-            "Sadly, not.",
+            "Signs point to yes.",
+            "Reply hazy, try again.",
+            "Ask again later.",
+            "Better not tell you now.",
+            "Cannot predict now.",
+            "Concentrate and ask again.",
+            "Don't count on it.",
+            "My reply is no.",
+            "My sources say no.",
+            "Outlook not so good.",
+            "Very doubtful."
         ];
         let response = responses[Math.floor(Math.random() * responses.length)];
 
