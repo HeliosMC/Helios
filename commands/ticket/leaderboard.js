bf35bb7492117ea7ff830c45e54234b3f4d9c5f7
AAigars <datorugames@gmail.com>
2021-03-21 14:43:30 +0000

    Remove staff check as it wasn't complete.

--- commands/ticket/leaderboard.js
+++ commands/ticket/leaderboard.js
@@ -45,12 +45,6 @@ module.exports = {
                 : 1;
         });
 
-        // Check if the user in the leaderboard is still staff.
-        leaderboard.map(entry => {
-            let member = await msg.guild.members.fetch(entry.closedBy);
-            if(!member) return;
-        });
-
         // Create a leaderboard array.
         let data = Object.keys(leaderboard).map((key) => {
             return [key, leaderboard[key]];
