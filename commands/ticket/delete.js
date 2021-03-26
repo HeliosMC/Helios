d1f3cc8ddfda50aa8b1f3714efe52059683cfaf7
AAigars <datorugames@gmail.com>
2021-03-26 18:18:57 +0000

    Remove the await from fetching the user.

--- commands/ticket/delete.js
+++ commands/ticket/delete.js
@@ -97,7 +97,7 @@ module.exports = {
                 msg.channel.name,
                 "https://cdn.discordapp.com/avatars/771824383429050379/4c48fcc72ea0640c9a1b8709770f41bc.png"
             );
-        await msg.guild.members
+        msg.guild.members
             .fetch(ticketChannel.userId)
             .then((member) => member.send(closedEmbed));
 
