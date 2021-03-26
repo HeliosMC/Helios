bfba60b8dd339c94e7d1e6f483cca003f5f0385a
AAigars <datorugames@gmail.com>
2021-03-26 18:16:05 +0000

    Handle the promise properly.

--- commands/ticket/delete.js
+++ commands/ticket/delete.js
@@ -97,7 +97,9 @@ module.exports = {
                 msg.channel.name,
                 "https://cdn.discordapp.com/avatars/771824383429050379/4c48fcc72ea0640c9a1b8709770f41bc.png"
             );
-        await msg.guild.members.fetch(ticketChannel.userId).send(closedEmbed);
+        await msg.guild.members
+            .fetch(ticketChannel.userId)
+            .then((member) => member.send(closedEmbed));
 
         // Log to transcript channel.
         const ticketEmbed = new Discord.MessageEmbed()
